7f3877b33096bbffc9053ccd336523b7595c8862
Alex Janakos <ajanakos@ucsc.edu>
2015-04-17 12:47:21 -0700

    not ready
    ..

--- website/admin/logout.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-session_start();
-
-unset($_SESSION['username']); // take out username key
-
-header('Location: login.php'); // return to login page
-
-?>
--- website/admin/template/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="footer">
-      <div class="container">
-        <p class="text-muted">Alex Janakos' Web Page</p>
-      </div>
-    </footer>
